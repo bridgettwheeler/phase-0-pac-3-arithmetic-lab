abea5ec097e3f70d1c89ad8bd5fd3842b1174037
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-06-08 10:01:22 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,63 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    return a += b;
+}
+add();
+
+function subtract(a, b) {
+    return a -= b;
+}
+subtract();
+
+function multiply(a, b) {
+    return a *= b;
+}
+multiply();
+
+function divide(a, b) {
+    return a /= b;
+}
+divide();
+
+function increment(n) {
+    n++;
+    return n;
+}
+increment();
+
+function decrement(n) {
+    n--;
+    return n;
+}
+decrement();
+
+function makeInt(n) {
+    parseInt(n, 10);
+    return parseInt(n, 10);
+
+}
+//makeInt();
+
+function preserveDecimal(n) {
+    parseFloat(n);
+    return parseFloat(n);
+
+}
+//preserveDecimal();
+
+
